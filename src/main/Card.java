7f66b3a317a5a917815b524e35dc0419ae84af68
Lucas Combs <l-combs@hotmail.com>
2018-09-16 19:33:24 -0400

    Formatted code

--- src/main/Card.java
+++ src/main/Card.java
@@ -3,53 +3,54 @@ package main;
 public class Card {
     private CardValue value;
     private Suit suit;
+
     /*
         String will be in format: AH
         A = Ace = Value
         H = Suit = Hearts
      */
     public Card(String card) {
-        this.value = CardValue.lookupByAbbreviation(card.substring(0,card.length()-1));
-        this.suit = Suit.valueOf(card.substring(card.length()-1));
+        this.value = CardValue.lookupByAbbreviation(card.substring(0, card.length() - 1));
+        this.suit = Suit.valueOf(card.substring(card.length() - 1));
     }
 
-    public CardValue getValue(){
+    public CardValue getValue() {
         return this.value;
     }
 
-    public Suit getSuit(){
+    public Suit getSuit() {
         return this.suit;
     }
 
-    public enum CardValue{
-        TWO ("2"),
-        THREE ("3"),
-        FOUR ("4"),
-        FIVE ("5"),
-        SIX ("6"),
-        SEVEN ("7"),
-        EIGHT ("8"),
-        NINE ("9"),
-        TEN ("10"),
-        JACK ("J"),
-        QUEEN ("Q"),
-        KING ("K"),
-        ACE ("A");
+    public enum CardValue {
+        TWO("2"),
+        THREE("3"),
+        FOUR("4"),
+        FIVE("5"),
+        SIX("6"),
+        SEVEN("7"),
+        EIGHT("8"),
+        NINE("9"),
+        TEN("10"),
+        JACK("J"),
+        QUEEN("Q"),
+        KING("K"),
+        ACE("A");
 
         // Store the above "A", "8", "K", ext.
         private String abbreviation;
 
         // Constructor for the above
-        CardValue(String abbreviation){
+        CardValue(String abbreviation) {
             this.abbreviation = abbreviation;
         }
 
         // Given "K" returns CardValue.KING
-        public static CardValue lookupByAbbreviation(String abbreviation){
+        public static CardValue lookupByAbbreviation(String abbreviation) {
 
             // Loop through values, return as soon as match is found
-            for(CardValue cv: CardValue.values()){
-                if(cv.abbreviation.equals(abbreviation))
+            for (CardValue cv : CardValue.values()) {
+                if (cv.abbreviation.equals(abbreviation))
                     return cv;
             }
 
@@ -58,11 +59,11 @@ public class Card {
         }
     }
 
-    public enum Suit{
-        H ("Hearts"),
-        D ("Diamonds"),
-        C ("Clubs"),
-        S ("Spades");
+    public enum Suit {
+        H("Hearts"),
+        D("Diamonds"),
+        C("Clubs"),
+        S("Spades");
 
         private String name;
 
--- src/test/HandTest.java
+++ src/test/HandTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HandTest {
     @Test
-    public void test_hand_creation_from_string(){
+    public void test_hand_creation_from_string() {
         Hand SUT = new Hand("2H 4S 4C 2D 4H");
         assertTrue(SUT != null);
     }
