8c427c6d1745eceaa0b5a1d68670bb233fc498a6
Lucas Combs <l-combs@hotmail.com>
2018-09-16 20:56:10 -0400

    PASSED: finding high card in hand

--- src/main/Hand.java
+++ src/main/Hand.java
@@ -1,7 +1,19 @@
 package main;
 
-public class Hand {
-    public Hand(String s) {
+import java.util.Arrays;
 
+public class Hand {
+    Card cards[] = new Card[5];
+
+    public Hand(String s) {
+        String cardStrings[] = s.split(" ");
+        for (int i = 0; i < cardStrings.length; i++) {
+            this.cards[i] = new Card(cardStrings[i]);
+        }
+    }
+
+    public Card getHighCard() {
+        Arrays.sort(cards);
+        return cards[0];
     }
 }
--- src/test/HandTest.java
+++ src/test/HandTest.java
@@ -1,14 +1,29 @@
 package test;
 
+import main.Card;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import main.Hand;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HandTest {
+    Hand SUT;
+
+    @BeforeEach
+    public void setup() {
+        SUT = null;
+    }
+
     @Test
     public void test_hand_creation_from_string() {
-        Hand SUT = new Hand("2H 4S 4C 2D 4H");
+        SUT = new Hand("2H 4S 4C 2D 4H");
         assertTrue(SUT != null);
+    }
+
+    @Test
+    public void test_high_card_in_hand() {
+        SUT = new Hand("2H 2D 5C 2S AS");
+        assertEquals(SUT.getHighCard(), new Card("AS"));
     }
 }
