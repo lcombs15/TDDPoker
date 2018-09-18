cafe209b2591a20276e0aa162c68fcbc16433b9a
Lucas Combs <l-combs@hotmail.com>
2018-09-17 22:13:49 -0400

    Renamed constructor variable to make more sense

--- src/main/Hand.java
+++ src/main/Hand.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Hand {
     Card cards[] = new Card[5];
 
-    public Hand(String s) {
-        String cardStrings[] = s.split(" ");
+    public Hand(String hand) {
+        String cardStrings[] = hand.split(" ");
         for (int i = 0; i < cardStrings.length; i++) {
             this.cards[i] = new Card(cardStrings[i]);
         }
