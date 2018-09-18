4be7db5a5185f13c84ffc4c6e7814e76476cf116
Lucas Combs <l-combs@hotmail.com>
2018-09-17 22:49:33 -0400

    PASSED: Throwing exceptions for invalid hands 	-null hand 	-invalid number of cards (tested too many & not enough) 	-invalid card in hand (with correct # cards)

--- src/main/Hand.java
+++ src/main/Hand.java
@@ -6,7 +6,14 @@ public class Hand {
     Card cards[] = new Card[5];
 
     public Hand(String hand) {
+        if(hand == null)
+            throw new IllegalArgumentException("Hand string cannot be null");
+
         String cardStrings[] = hand.split(" ");
+
+        if(cardStrings.length != 5)
+            throw new IllegalArgumentException("Hand string must contain 5 cards. Given: " + hand);
+
         for (int i = 0; i < cardStrings.length; i++) {
             this.cards[i] = new Card(cardStrings[i]);
         }
--- src/test/HandTest.java
+++ src/test/HandTest.java
@@ -22,6 +22,33 @@ public class HandTest {
     }
 
     @Test
+    public void givenHandStringOfIncorrectLength_whenPassedToConstructor_throwIllegalArgumentException(){
+        // Hand string too long
+        assertThrows(IllegalArgumentException.class, () -> {
+            SUT = new Hand("2H 4S AS AD AC AH");
+        });
+
+        // Hand string too short
+        assertThrows(IllegalArgumentException.class, () -> {
+            SUT = new Hand("2H 4S");
+        });
+    }
+
+    @Test
+    public void whenHandStringNull_givenHandConstructor_throwIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SUT = new Hand(null);
+        });
+    }
+
+    @Test
+    public void whenCorrectLengthHandStringContainsInvalidCard_givenHandConstructor_throwIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SUT = new Hand("2H 8R 2S AS AD");
+        });
+    }
+
+    @Test
     public void test_high_card_in_hand() {
         SUT = new Hand("2H 2D 5C 2S AS");
         assertEquals(SUT.getHighCard(), new Card("AS"));
