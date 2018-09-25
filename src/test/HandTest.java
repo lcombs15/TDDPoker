3c07f9de404c47ac45ff8cb3dce9f08a6f6ea569
Lucas Combs <l-combs@hotmail.com>
2018-09-25 14:31:05 -0400

    Code formatting and comments for test groups

--- src/test/HandTest.java
+++ src/test/HandTest.java
@@ -16,6 +16,9 @@ public class HandTest {
         SUT = null;
     }
 
+    /**
+     * <Invalid input testing>
+     */
     @Test
     public void givenValidHandString_whenPassedToConstructor_HandObjectIsNotNull() {
         SUT = new Hand("2H 4S 4C 2D 4H");
@@ -23,7 +26,7 @@ public class HandTest {
     }
 
     @Test
-    public void givenHandStringOfIncorrectLength_whenPassedToConstructor_throwIllegalArgumentException(){
+    public void givenHandStringOfIncorrectLength_whenPassedToConstructor_throwIllegalArgumentException() {
         // Hand string too long
         assertThrows(IllegalArgumentException.class, () -> {
             SUT = new Hand("2H 4S AS AD AC AH");
@@ -49,55 +52,66 @@ public class HandTest {
         });
     }
 
+    /**
+     * </Invalid input testing>
+     */
+
     @Test
     public void givenValidHandWithHighCardAceSpades_whenGetHighCardCalled_returnAceSpades() {
         SUT = new Hand("2H 2D 5C 2S AS");
-        assertEquals(new Card("AS"),SUT.getHighCard());
+        assertEquals(new Card("AS"), SUT.getHighCard());
     }
 
     @Test
-    public void givenHandWithTwoEqualValueCards_whenHasTwoOfKindCalled_thenTrue(){
+    public void givenHandWithTwoEqualValueCards_whenHasTwoOfKindCalled_thenTrue() {
         SUT = new Hand("4C 5H 2D AS AH");
         assertTrue(SUT.isTwoOfKind());
     }
 
     @Test
-    public void givenHandWithUniqueValueCards_whenIsTwoOfKindCalled_thenFalse(){
+    public void givenHandWithUniqueValueCards_whenIsTwoOfKindCalled_thenFalse() {
         SUT = new Hand("AS KH 4C 5H 2D");
         assertFalse(SUT.isTwoOfKind());
     }
 
-    @Test void givenStraightHandWithoutAce_whenIsStraightCalled_thenTrue(){
+    @Test
+    void givenStraightHandWithoutAce_whenIsStraightCalled_thenTrue() {
         SUT = new Hand("QD KC JH 10D 9S");
         assertTrue(SUT.isStraight());
     }
 
-    @Test void givenStraightHandWithAceHigh_whenIsStraightCalled_thenTrue(){
+    @Test
+    void givenStraightHandWithAceHigh_whenIsStraightCalled_thenTrue() {
         SUT = new Hand("QD KC AS JH 10D");
         assertTrue(SUT.isStraight());
     }
 
-    @Test void givenStraightHandWithAceLow_whenIsStraightCalled_thenTrue(){
+    @Test
+    void givenStraightHandWithAceLow_whenIsStraightCalled_thenTrue() {
         SUT = new Hand("4H 3H 2D AS 5C");
         assertTrue(SUT.isStraight());
     }
 
-    @Test void givenStraightHandWithAceHighAndLow_whenIsStraightCalled_thenFalse(){
+    @Test
+    void givenStraightHandWithAceHighAndLow_whenIsStraightCalled_thenFalse() {
         SUT = new Hand("QD KD AS 2C 3H");
         assertFalse(SUT.isStraight());
     }
 
-    @Test void givenHandWithAllTenAndJack_whenIsStraightCalled_thenFalse(){
+    @Test
+    void givenHandWithAllTenAndJack_whenIsStraightCalled_thenFalse() {
         SUT = new Hand("10H 10C 10D JS JC");
         assertFalse(SUT.isStraight());
     }
 
-    @Test void givenHandWithAllAcesAndATwo_whenIsStraightCalled_thenFalse(){
+    @Test
+    void givenHandWithAllAcesAndATwo_whenIsStraightCalled_thenFalse() {
         SUT = new Hand("AS 2H AC AD AH");
         assertFalse(SUT.isStraight());
     }
 
-    @Test void givenHandWithATwoAThreeAndThreeAces_whenIsStraightCalled_thenFalse(){
+    @Test
+    void givenHandWithATwoAThreeAndThreeAces_whenIsStraightCalled_thenFalse() {
         SUT = new Hand("AS 2H AC AD 3H");
         assertFalse(SUT.isStraight());
     }
