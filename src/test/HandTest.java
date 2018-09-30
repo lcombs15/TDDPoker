8ac9bb41b4c4125bde097992989959d0778c4669
Lucas Combs <l-combs@hotmail.com>
2018-09-29 21:13:38 -0400

    Formatted code in HandTest.java

--- src/test/HandTest.java
+++ src/test/HandTest.java
@@ -1,11 +1,9 @@
 package test;
 
 import main.Card;
+import main.Hand;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import main.Hand;
-
-import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -178,40 +176,40 @@ public class HandTest {
     }
 
     @Test
-    public void givenHandWithStraightFlush_whenGetScoreCalled_thenReturnsStraightFlush(){
+    public void givenHandWithStraightFlush_whenGetScoreCalled_thenReturnsStraightFlush() {
         SUT = new Hand("AS KS QS JS 10S");
         assertEquals(Hand.Score.StraightFlush, SUT.getScore());
     }
 
     @Test
-    public void givenFlush_whenComparedToPair_thenOne(){
+    public void givenFlush_whenComparedToPair_thenOne() {
         SUT = new Hand("5H 2H 7H 9H JH");
         Hand that = new Hand("4H 4D 3C 7S 8C");
         assertEquals(1, SUT.compareTo(that));
     }
 
     @Test
-    public void givenX_whenComparedToX_thenZero(){
+    public void givenX_whenComparedToX_thenZero() {
         SUT = new Hand("5H 2H 7H 9H JH");
         assertEquals(0, SUT.compareTo(SUT));
     }
 
     @Test
-    public void givenTwoStraightsXY_whenXHighCompareToYLow_thenOne(){
+    public void givenTwoStraightsXY_whenXHighCompareToYLow_thenOne() {
         SUT = new Hand("AS KH QD JS 10H");
         Hand other = new Hand("KH QD JS 10H 9S");
         assertEquals(1, SUT.compareTo(other));
     }
 
     @Test
-    public void givenStraightY_whenWinningHandAgainstFlushX_thenReturnX(){
+    public void givenStraightY_whenWinningHandAgainstFlushX_thenReturnX() {
         SUT = new Hand("AS KD QC JD 10H");
         Hand opponent = new Hand("3H 2H 8H 10H JH");
-        assertEquals(opponent,SUT.winningHandAgainst(opponent));
+        assertEquals(opponent, SUT.winningHandAgainst(opponent));
     }
 
     @Test
-    public void givenStraight_whenWinningHandAgainstItSelf_thenReturnNull(){
+    public void givenStraight_whenWinningHandAgainstItSelf_thenReturnNull() {
         SUT = new Hand("AS KD QC JD 10H");
         assertNull(SUT.winningHandAgainst(SUT));
     }
